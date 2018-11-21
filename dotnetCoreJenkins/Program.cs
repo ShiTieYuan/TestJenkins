b4d947d6db5291902527b7bc1fbabe4efb482572
unknown <shitieyuan1988@outlook.com>
2018-11-21 20:38:51 +0800

    提交测试

--- dotnetCoreJenkins/Program.cs
+++ dotnetCoreJenkins/Program.cs
@@ -6,7 +6,7 @@ namespace dotnetCoreJenkins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
             Console.Read();
         }
     }
